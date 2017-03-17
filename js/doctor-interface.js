26338a5ccd121016d237f551feb8f8bd5a0264cc
Kory <koryskarbek@yahoo.com>
2017-03-17 12:10:47 -0700

    add functions doctorInfo and doctorlanguages

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,24 +1,38 @@
 var apiKey = require('./../.env').apiKey;
 var getDoctors = require('./../js/doctor.js').getDoctorsModule;
+var getDoctor = require('./../js/doctor.js').getDoctorModule;
+
+
+function doctorInfo(image, degree) {
+  console.log(image);
+  $('#image').html("<img src='" + image + "'>")
+  $('#degree').text(degree);
+}
+
+function doctorLanguages(languages){
+  $("#languages").empty();
+  languages.forEach(function(language) {
+    console.log(language.name);
+    $("#languages").append(language.name + " | ");
+  });
+}
 
 function allDoctors(results){
   $("#doctors").empty();
   $("#practice").empty();
   results.data.forEach(function(result) {
-    $('#doctors').append("<br><button id=\"" + result.profile.first_name + " " + result.profile.last_name + "\" class='btn doctor-info' type='button' value=\"" + "\">" + result.profile.first_name + " " + result.profile.last_name + "</button><br>");
+    var name = result.profile.first_name + " " + result.profile.last_name;
+    $('#doctors').append("<br><button id=\"" + name + "\" class='btn doctor-info' type='button' value=\"" + name + "\">" + name + "</button><br>");
   });
   $('.doctor-info').click(function() {
     $('#result').show();
-    console.log($(this).val());
+    var nameArray = $(this).val().split(' ');
+    // console.log(nameArray[0]);
     $('#doctor-name').text($(this).val());
-    // getDoctor($(this).val());
+    getDoctor(nameArray[0], nameArray[1], doctorInfo, doctorLanguages);
   });
 }
 
-// function doctorInfo(image, degree, language) {
-//   $('#image').html("<img src='" + image + "'>")
-//   $('#degree').text(degree);
-// }
 
 $(document).ready(function() {
   $('form').submit(function() {
--- js/doctor.js
+++ js/doctor.js
@@ -4,26 +4,29 @@ var getDoctors = function(medicalIssue, allDoctors) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + medicalIssue + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
 
    .then(function(results) {
-      console.log(results);
+      // console.log(results);
       allDoctors(results);
     })
    .fail(function(error){
       console.log("fail");
     });
 };
-// var getDoctor = function(first, last) {
-//   $.get('https://api.betterdoctor.com/2016-03-01/doctors?first_name=' + first + '&last_name=' + last + '&skip=0&limit=10&user_key=' + apiKey)
-//
-//    .then(function(results) {
-//       console.log(results);
-//
-//     })
-//    .fail(function(error){
-//       console.log("fail");
-//     });
-// };
+var getDoctor = function(first, last, doctorInfo, doctorLanguages) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?first_name=' + first + '&last_name=' + last + '&skip=0&limit=10&user_key=' + apiKey)
+
+   .then(function(results) {
+     var currentDoctor = results.data[0];
+      doctorInfo(currentDoctor.profile.image_url, currentDoctor.educations[0].school);
+      doctorInfo(currentDoctor.profile.languages);
+
+    })
+   .fail(function(error){
+      console.log("fail");
+    });
+};
 
 
 // var getInfo = function()
 
 exports.getDoctorsModule = getDoctors;
+exports.getDoctorModule = getDoctor;
