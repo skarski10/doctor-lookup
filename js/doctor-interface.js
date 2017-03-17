570caefa599350c040cfdc0941824de43cfe325e
Kory <koryskarbek@yahoo.com>
2017-03-17 10:32:35 -0700

    add allDoctors and getDoctors functions

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,2 +1,18 @@
 var apiKey = require('./../.env').apiKey;
 var getDoctors = require('./../js/doctor.js').getDoctorsModule;
+
+function allDoctors(results){
+  $("#doctors").empty();
+  $("#practice").empty();
+  results.data.forEach(function(result) {
+    $('#practice').append("<br><button id=\"" + result.profile.first_name + " " + result.profile.last_name + "\" class='btn page-info' type='button' value=\"" + "\">" + result.profile.first_name + " " + result.profile.last_name + "</button><br>");
+  });
+}
+
+$(document).ready(function() {
+  $('form').submit(function() {
+    event.preventDefault();
+    console.log($('#symptom').val());
+    getDoctors($('#symptom').val(), allDoctors);
+  });
+});
--- js/doctor.js
+++ js/doctor.js
@@ -1,13 +1,14 @@
 var apiKey = require('./../.env').apiKey;
 
-getDoctors = function(medicalIssue) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
-   .then(function(result) {
-      console.log(result);
+getDoctors = function(medicalIssue, allDoctors) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + medicalIssue + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+   .then(function(results) {
+      console.log(results);
+      allDoctors(results);
     })
    .fail(function(error){
       console.log("fail");
     });
 };
 
-export.getDoctorsModule = getDoctors;
+exports.getDoctorsModule = getDoctors;
